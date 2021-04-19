83060d45cbb99423489cd65d016d4472f010e9bc
sialot <sialot@sina.com>
2021-04-19 14:27:08 +0800

    无用代码删除

--- src/main/ui/TitleBar.d.ts
+++
@@ -1,7 +0,0 @@
-import { BrowserWindow } from 'electron';
-declare function doActions(win: BrowserWindow, action: string): void;
-declare const _default: {
-    doActions: typeof doActions;
-};
-export default _default;
-//# sourceMappingURL=TitleBar.d.ts.map
