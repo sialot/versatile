6dbbb17025294890ad79a806e098c069057f141c
sialot <sialot@sina.com>
2021-04-23 14:12:03 +0800

    删除无用属性

--- src/renderer/store/modules/data-viewer.ts
+++ src/renderer/store/modules/data-viewer.ts
@@ -3,18 +3,15 @@ import * as types from '@/store/mutation-types'
 
 // 菜单数据定义
 export interface State {
-    title:string
 }
 
 // 初始化数据
 const initState: State = {
-    title : 'State Viewer'
 }
 
 
 // getters
 const getters = {
-    title: (state: State) => state.title,
 }
   
 // actions
